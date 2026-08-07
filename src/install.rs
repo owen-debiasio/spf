efaa08301f3835564991988a023aa12561a0e538
Owen <owen.debiasio@gmail.com>
2026-08-07 19:24:17 -0400

    Rename variables

--- src/install.rs
+++ src/install.rs
@@ -6,19 +6,19 @@ use crate::error;
 pub fn spf_install(spf_package_path: String) {
     check_package_name(spf_package_path.clone());
 
-    println!("Installing: {spf_package_path:?}")
+    println!("Installing: {spf_package_path}")
 }
 
 /// Makes sure the file name is compatible. Manually
 /// parses the string. Super inefficient lol
-fn check_package_name(name: String) {
+fn check_package_name(package_path: String) {
     // if file exists
-    if !Path::new(&name).exists() {
-        error(&format!("File not found: {name:?}"))
+    if !Path::new(&package_path).exists() {
+        error(&format!("File not found: {package_path}"))
     }
 
     // if file ends with .spf
-    if !name.ends_with(".spf") {
+    if !package_path.ends_with(".spf") {
         error("Not .spf")
     }
 }
