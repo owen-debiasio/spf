87efcac11d473d743a3f2573e72c4758d62f0ad9
Owen <owen.debiasio@gmail.com>
2026-08-08 17:13:02 -0400

    Add init for the packages location

---
+++ src/init.rs
@@ -0,0 +1,22 @@
+use is_root::is_root;
+use std::{fs::create_dir_all, path::Path};
+
+use crate::error;
+
+pub fn init() {
+    let paths_to_check = vec!["/usr/share/spf/packages/"];
+
+    for path in paths_to_check {
+        if !Path::new(path).exists() {
+            if !is_root() {
+                error("In order to initialize the filesystem, you must run spf as root")
+            }
+
+            create_dir_all(path).unwrap_or_else(|err| {
+                error(&format!(
+                    "Failed init spf: Failed to create directory \"{path}\": {err}"
+                ))
+            });
+        }
+    }
+}
