585188e59b3045f4ca68987f33fcc541f668c960
Owen <owen.debiasio@gmail.com>
2026-08-10 12:22:10 -0400

    Move to ./dev

--- dev/install.bash
+++ dev/install.bash
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+set -e
+
+# Update rust
 rustup update
 cargo update
 
+# lint and format
 cargo clippy
 cargo fmt
+
+# build
 cargo build
 
-sudo cp target/debug/spf /usr/bin/spf
+# copy spf
+sudo cp ./target/debug/spf /usr/bin/spf
 
 echo -e "\ninstalled"
