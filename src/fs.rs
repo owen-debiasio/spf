c9a9305773885fd99721dabd4d2f6df0699157f0
Owen <owen.debiasio@gmail.com>
2026-08-10 11:53:42 -0400

    Fix archive not being created

--- src/fs.rs
+++ src/fs.rs
@@ -34,18 +34,32 @@ impl FileProperty {
 /// Why am I using `std::process::command` instead of a crate? F*ck you, that's why
 /// I'm using `tar` because it's basically on every distro and supported well on Linux.
 pub fn create_archive_of_dir(parent_directories: &str, output: &str, directory: &str) {
-    Command::new("tar")
-        .arg("-C")
-        .arg(parent_directories)
-        .arg("-cvf")
-        .arg(output)
-        .arg(directory)
-        .output()
-        .unwrap_or_else(|err| {
-            error(&format!(
-                "Failed to create archive of dir \"{directory}\" to \"{output}\": {err}"
-            ))
-        });
+    // You gotta make this because if `parent_directories` it will prob shit itself smh
+    if parent_directories.is_empty() {
+        Command::new("tar")
+            .arg("-cf")
+            .arg(output)
+            .arg(directory)
+            .output()
+            .unwrap_or_else(|err| {
+                error(&format!(
+                    "Failed to create archive of dir \"{directory}\" to \"{output}\": {err}"
+                ))
+            });
+    } else {
+        Command::new("tar")
+            .arg("-C")
+            .arg(parent_directories)
+            .arg("-cf")
+            .arg(output)
+            .arg(directory)
+            .output()
+            .unwrap_or_else(|err| {
+                error(&format!(
+                    "Failed to create archive of dir \"{directory}\" to \"{output}\": {err}"
+                ))
+            });
+    }
 }
 
 /// Creates an archive of a directory.
