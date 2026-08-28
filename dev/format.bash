0505a12045ebf8d2d74ce669d984ebfedd0be239
Owen Debiasio <owen.debiasio@gmail.com>
2026-08-28 10:46:00 -0400

    Use config in rust-toolchain.toml

--- dev/format.bash
+++ dev/format.bash
@@ -11,8 +11,5 @@ cargo update
 cargo clippy
 cargo fmt
 
-rustup target add aarch64-unknown-linux-gnu
-rustup target add x86_64-unknown-linux-gnu
-
 find . -name "*.bash" -exec shellcheck {} +
 shfmt -w -i 4 -ci -sr .
