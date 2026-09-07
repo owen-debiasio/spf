927d772fdca3ad09e15c4f435329f1984427bfa1
Owen DeBiasio <owen.debiasio@gmail.com>
2026-09-06 22:14:22 -0400

    Added metadata collection, adding metadata conversion next

--- src/convert.rs
+++ src/convert.rs
@@ -4,9 +4,15 @@
 //! Copyright (C) 2026 Owen Debiasio <owen.debiasio@gmail.com>
 //! SPDX-License-Identifier: GPL-3.0-or-later
 
-use std::path::Path;
+use std::{
+    fs::{self, remove_file},
+    path::Path,
+};
 
-use crate::{fs::extract_archive, sys::error};
+use crate::{
+    fs::{FileProperty, extract_archive},
+    sys::error,
+};
 
 pub fn convert(source_package_path: &str, output_package_path: &str) -> Result<(), std::io::Error> {
     /* Input file checks */
@@ -21,11 +27,10 @@ pub fn convert(source_package_path: &str, output_package_path: &str) -> Result<(
         error(&format!("File \"{source_package_path}\" does not exist!"))
     }
 
-    let source_file_ext = source_file
-        .extension()
-        .unwrap_or_default()
-        .display()
-        .to_string();
+    let source_file_ext = match FileProperty::extension(source_package_path) {
+        Ok(ext) => ext,
+        Err(err) => error(&format!("Failed to get source file extension: {err}")),
+    };
 
     if !matches!(source_file_ext.as_str(), "spf" | "deb") {
         error(&format!(
@@ -39,13 +44,10 @@ pub fn convert(source_package_path: &str, output_package_path: &str) -> Result<(
         error("Please provide an output package path!")
     }
 
-    let output_file = Path::new(output_package_path);
-
-    let output_file_ext = output_file
-        .extension()
-        .unwrap_or_default()
-        .display()
-        .to_string();
+    let output_file_ext = match FileProperty::extension(output_package_path) {
+        Ok(ext) => ext,
+        Err(err) => error(&format!("Failed to get output file extension: {err}")),
+    };
 
     if !matches!(output_file_ext.as_str(), "spf" | "deb") {
         error(&format!(
@@ -54,6 +56,8 @@ pub fn convert(source_package_path: &str, output_package_path: &str) -> Result<(
     }
 
     println!("Converting \"{source_package_path}\" -> \"{output_package_path}\"...");
+
+    let source_is_debian = source_file_ext == "deb";
 
     // Dynamically choose the extractor for the package.
     //
@@ -71,6 +75,46 @@ pub fn convert(source_package_path: &str, output_package_path: &str) -> Result<(
     println!("    Extracting \"{source_package_path}\"...");
 
     extract_archive(extracter, source_package_path)?;
+
+    // `debian-binary` isn't used, so delete it
+    if source_is_debian {
+        remove_file("debian-binary")?;
+    }
+
+    /* Start reading metadata */
+
+    println!("    Reading metadata...");
+
+    let spf_metadata_path = &FileProperty::name(source_package_path)?.replace(".spf", "/META");
+
+    // `control.tar.xz` contains the debian package metadata, so extract if applicable.
+    // Otherwise, extract assumed location of `.spf` package metadata, then collect the
+    // metadata.
+    let mut source_metadata: String = if source_is_debian {
+        println!("        Extracting \"control.tar.xz\"...");
+        extract_archive("tar", "control.tar.xz")?;
+
+        remove_file("control.tar.xz")?;
+
+        println!("        Reading \"control\"...");
+
+        fs::read_to_string("control")?
+    } else {
+        println!("        Reading \"{spf_metadata_path}\"...");
+        fs::read_to_string(spf_metadata_path)?
+    };
+
+    println!("        Cleaning up metadata collection...");
+
+    if source_is_debian {
+        remove_file("control")?;
+    } else {
+        remove_file(spf_metadata_path)?;
+    }
+
+    /* Conversion of metadata */
+
+    println!("        Converting metadata...");
 
     Ok(())
 }
